36153c948cca05ef2839fa573a6378109a0126ba
riushandy <riushdanylintar@gmail.com>
2026-04-20 09:51:40 +0700

    pertemuan 7

---
+++ app/Http/Controllers/DosenController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+//package kuliah.pert3
+
+use Illuminate\Http\Request;
+// import
+
+class DosenController extends Controller
+{
+    //
+
+public function index(){
+    return "<h1>Halo ini adalah method index, dalam controller DosenController. - www.malasngoding.com<h1>";
+}
+
+public function biodata(){
+        $name = "riu";
+    	$pelajaran = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
+        return view('biodata',['name' => $name , 'matkul' => $pelajaran]);
+    }
+
+}
+
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang</h1> di tutorial laravel pi>www.malasngoding.com</i>";
+});
+
+Route::get('blog', function () {
+    return view('blog');
+});
+
+Route::get('pertemuan5', function () {
+    return view('pertemuan5');
+});
+
+Route::get('dosen', [DosenController::class, 'index']);
+
+Route::get('biodata', [DosenController::class, 'biodata']);
